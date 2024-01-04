ea3dfa2f17acd01f4bd15583cb5bc11c38a2e0d7
Atit Kunwar <Atitkunwar2015@gmail.com>
2024-01-04 21:57:46 +0545

    gg

--- tools/splitImg.py
+++ tools/splitImg.py
@@ -3,19 +3,20 @@ import shutil
 from sklearn.model_selection import train_test_split
 
 def split_data(source_folder, destination_folder):
-    # Create train, test, and validation folders
+    # make test,train validation directories
     train_folder = os.path.join(destination_folder, 'train')
     test_folder = os.path.join(destination_folder, 'test')
-    validation_folder = os.path.join(destination_folder, 'validation')
-
+    validation_folder = os.path.join(destination_folder, 'valid')
     os.makedirs(train_folder, exist_ok=True)
     os.makedirs(test_folder, exist_ok=True)
     os.makedirs(validation_folder, exist_ok=True)
 
-    # Function to move files to their respective folders
+
+
     def move_files(files, source, destination):
         os.makedirs(destination, exist_ok=True)
-        # Create a subdirectory named 'images' inside the destination folder
+       
+ 
         images_subfolder = os.path.join(destination, 'images')
         os.makedirs(images_subfolder, exist_ok=True)
 
@@ -26,7 +27,7 @@ def split_data(source_folder, destination_folder):
     image_files = [f for f in os.listdir(source_folder) if f.endswith('.jpg') or f.endswith('.png')]
 
     # Split the files into train, test, and validation sets
-    train_files, test_validation_files = train_test_split(image_files, test_size=0.4, random_state=42)
+    train_files, test_validation_files = train_test_split(image_files, test_size=0.2, random_state=42)
     test_files, validation_files = train_test_split(test_validation_files, test_size=0.5, random_state=42)
 
     # Move image files to the 'images' subdirectory in their respective folders
@@ -49,11 +50,8 @@ def split_data(source_folder, destination_folder):
                 shutil.move(text_file_path, labels_subfolder)
 
 if __name__ == "__main__":
-    # Set the path to the SampleImages/images folder
-    source_images_folder = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'SampleImages', 'images')
 
-    # Set the path to the SampleImages folder
-    destination_folder = os.path.join(source_images_folder, '..')
-
-    # Call the split_data function
-    split_data(source_images_folder, destination_folder)
+    Sample_Images= os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'SampleImages', 'images')
+    Splited_Data = os.path.join(Sample_Images, '..')
+    
+    split_data(Sample_Images, Splited_Data)
